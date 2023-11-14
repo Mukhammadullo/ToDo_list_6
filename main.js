c9e330ca0afbd6fcf9eea39b411e815317e098e5
Muhammadullo <mnastulloev@gmail.com>
2023-11-14 23:04:18 +0500

    getData

---
+++ main.js
@@ -0,0 +1,41 @@
+let tbody = document.querySelector(".tbody")
+let url = "https://655367345449cfda0f2ea0f3.mockapi.io/todo"
+
+async function getData() {
+    try {
+        let reponse = await fetch(url)
+        let data = await reponse.json()
+        get(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+getData()
+
+
+// get
+function get(newData) {
+    newData.forEach(element => {
+
+        let tr = document.createElement("tr")
+
+        // title
+        let forTitle = document.createElement("h2")
+        forTitle.innerHTML = element.title
+
+        // forName
+        let forName = document.createElement("h3")
+        forName.innerHTML = element.name
+
+        // forAvatar
+        let forAvatar = document.createElement("img")
+        forAvatar.src = element.avatar
+
+        let card = document.createElement("div")
+        card.append(forName, forTitle, forAvatar)
+
+        tbody.appendChild(card)
+    });
+}
+
+
